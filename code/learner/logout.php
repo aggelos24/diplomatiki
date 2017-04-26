c63f4a0171e338896ab79e4a28a7a1cc13c4d057
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 00:49:10 +0300

    Update logout.php

--- code/learner/logout.php
+++ code/learner/logout.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <?php
-session_start();																							//δημιουργία συνεδρίας
-session_unset();																							//διαγραφή μεταβλητών συνεδρίας
-session_write_close();																						//διαγραφή συνεδρίας
+session_start();																					                                        		//δημιουργία συνεδρίας
+session_unset();															                                      		  						//διαγραφή μεταβλητών συνεδρίας
+session_write_close();																				                                    		//διαγραφή συνεδρίας
 echo "<script> alert('Αποσυνδέθηκες.'); location.href = '../login_register_form.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στη σελίδα login_register_form.php
 ?>
