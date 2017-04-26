62072754f53540d2f045aa746f047c4c92a13735
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 01:01:12 +0300

    Update not_display_notification.php

--- code/learner/not_display_notification.php
+++ code/learner/not_display_notification.php
@@ -1,19 +1,19 @@
 <meta charset="utf-8" />
 <?php
-session_start();																									//δημιουργία συνεδρίας
-if ((isset($_GET["id"])) and (isset($_GET["id"]))) {																//αν υπάρχει η μεταβλητή GET
-	$id = $_GET["id"];																								//ανάθεσή της σε μεταβλητή
+session_start();													//δημιουργία συνεδρίας
+if ((isset($_GET["id"])) and (isset($_GET["id"]))) {									//αν υπάρχει η μεταβλητή GET
+	$id = $_GET["id"];												//ανάθεσή της σε μεταβλητή
 }
-else {																												//αν όχι
-echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";							//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
+else {															//αν όχι
+echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
 }
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 													//απόπειρα σύνδεσης στη βάση
-if (!$link) {																										//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";						//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 							//απόπειρα σύνδεσης στη βάση
+if (!$link) {														//αν αποτυχία
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
-$link->query ("UPDATE notification SET display=0 WHERE id=".$id);													//ενημέρωση του πίνακα notification
-$link->close();																										//κλείσιμο σύνδεσης με βάση
+$link->query ("UPDATE notification SET display=0 WHERE id=".$id);							//ενημέρωση του πίνακα notification
+$link->close();														//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Η ειδοποίηση δεν θα εμφανιστεί ξανά.'); location.href = 'notification.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
 ?>
