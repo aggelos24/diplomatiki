6df4f2318d63f410cb1fa37bcd8bcd7f6bb3681b
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 00:24:07 +0300

    Update find_friend.php

--- code/learner/find_friend.php
+++ code/learner/find_friend.php
@@ -26,36 +26,36 @@
 		Φίλοι
 		<div>
 <?php
-include "if_not_logged_l.php";																						//έλεγχος αν έχει συνδεθεί μαθητής
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 													//απόπειρα σύνδεσης στη βάση
-if (!$link) {																										//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";								//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+include "if_not_logged_l.php";													//έλεγχος αν έχει συνδεθεί μαθητής
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 								//απόπειρα σύνδεσης στη βάση
+if (!$link) {															//αν αποτυχία
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";						//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT friendship.user2, user.photo FROM friendship INNER JOIN user ON friendship.user2=user.username WHERE friendship.user1='".$_SESSION["session_lusername"]."'");
-																													//ανάκτηση πληροφοριών φίλων του χρήστη
+																//ανάκτηση πληροφοριών φίλων του χρήστη
 $friends = array();
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {															//για κάθε φίλο του χρήστη
-	if (isset($row["photo"])) {																						//αν ο φίλος έχει ανεβάσει φωτογραφία
-		$fpath=$row["photo"];																						//εμφάνιση φωτογραφίας του
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {									//για κάθε φίλο του χρήστη
+	if (isset($row["photo"])) {												//αν ο φίλος έχει ανεβάσει φωτογραφία
+		$fpath=$row["photo"];												//εμφάνιση φωτογραφίας του
 	}
-	else {																											//αν όχι
-		$fpath="photos/profile.png";																				//θα εμφανίσει την default εικόνα
+	else {															//αν όχι
+		$fpath="photos/profile.png";											//εμφάνιση default εικόνας
 	}
-array_push($friends, $row["user2"]);																				//πρόσθεσε το username του φίλου στη μεταβλητή friends
+array_push($friends, $row["user2"]);												//προσθήκη του username του φίλου στον πίνακα friends
 echo "<div class='view_users'>"."<a href='view_profile.php?username=".$row["user2"]."&friend=1'><img src='".$fpath."' alt='Φωτογραφία Προφίλ' class='user_profile_photo'>"."</a>"."<p class='center'>".$row["user2"]."</p>"."</div>";
-																													//εμφάνιση εικόνας προφιλ του
+																//εμφάνιση εικόνας προφιλ του
 }
 ?>
 		</div>
 		Υπόλοιποι Χρήστες
 		<div>
 <?php
-$result = $link->query ("SELECT * FROM user WHERE professor=0");													//ανάκτηση πληροφοριών χρηστών πλην του καθηγητή
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {															//για κάθε χρήστη πλην του καθηγητή
+$result = $link->query ("SELECT * FROM user WHERE professor=0");								//ανάκτηση πληροφοριών χρηστών πλην του καθηγητή
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {									//για κάθε χρήστη πλην του καθηγητή
 	if (!(in_array($row["username"], $friends)) and ($row["username"] != $_SESSION["session_lusername"])) {			//αν δεν είναι ο συνδεδεμένος χρήστης ή φίλος του χρήστη
-		if (isset($row["photo"])) {																					//ομοίως
+		if (isset($row["photo"])) {											//ομοίως
 			$fpath = $row["photo"];
 		}
 		else {
@@ -65,7 +65,7 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {															//γι�
 	}
 }
 $result->free();
-$link->close();																										//κλείσιμο σύνδεσης με βάση
+$link->close();															//κλείσιμο σύνδεσης με βάση
 ?>
 		</div>
 	</div>
