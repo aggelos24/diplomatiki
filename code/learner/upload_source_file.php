fbc98dfcea5d57f560faa14b3994451ab0ad7bef
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 01:58:46 +0300

    Update upload_source_file.php

--- code/learner/upload_source_file.php
+++ code/learner/upload_source_file.php
@@ -5,44 +5,49 @@ $greek_alphabet = array ("α", "ά", "β", "γ", "δ", "ε", "έ", "ζ", "η", "
 $latin_repl = array ("a", "a", "b", "g", "d", "e", "e", "z", "i", "i", "th", "i", "i", "k", "l", "m", "n", "x", "o", "o", "p", "r", "s", "t", "u", "u", "f", "x", "ps", "o", "o", "s",
 "A", "A", "B", "G", "D", "E", "E", "Z", "I", "I", "Th", "I", "I", "K", "L", "M", "N", "X", "O", "O", "P", "R", "S", "T", "Y", "Y", "F", "X", "Y", "O", "O");
 $ok = 1;
-session_start();																												//δημιουργία συνεδρίας
-if (isset($_GET["id"])) {																										//αν υπάρχει η μεταβλητή GET
-	$id = $_GET["id"];																											//ανάθεσή της σε μεταβλητή
+session_start();									//δημιουργία συνεδρίας
+if (isset($_GET["id"])) {								//αν υπάρχει η μεταβλητή GET
+	$id = $_GET["id"];								//ανάθεσή της σε μεταβλητή
 }
-else {																															//αν δεν υπάρχει
+else {											//αν δεν υπάρχει
 	$ok = 0;
-	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";							//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";
+											//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 }
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 																//απόπειρα σύνδεσης στη βάση
-if (!$link) {																													//αν αποτυχία
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 			//απόπειρα σύνδεσης στη βάση
+if (!$link) {										//αν αποτυχία
 	$ok = 0;
-	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";							//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";
+											//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
-$target_file = "../projects/project_".$id."/".basename($_FILES["file"]["name"]);												//ορισμός διεύθυνσης προορισμού του αρχείου
-$target_file = str_replace($greek_alphabet, $latin_repl, $target_file);															//αντικατάσταση ελληνικών χαρακτήρων με λατινικών
-$result = $link->query ("SELECT * FROM source_file where path='".$target_file."'");												//έλεγχος αν υπάρχει αρχείο με αυτό το όνομα
-if (!empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {																		//αν υπάρχει
+$target_file = "../projects/project_".$id."/".basename($_FILES["file"]["name"]);	//ορισμός διεύθυνσης προορισμού του αρχείου
+$target_file = str_replace($greek_alphabet, $latin_repl, $target_file);			//αντικατάσταση ελληνικών χαρακτήρων με λατινικών
+$result = $link->query ("SELECT * FROM source_file where path='".$target_file."'");	//έλεγχος αν υπάρχει αρχείο με αυτό το όνομα
+if (!empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {				//αν υπάρχει
 	$ok = 0;
 	$result->free();
-	$link->close();																												//κλείσιμο σύνδεσης με βάση
-	echo "<script> alert('Υπάρχει αρχείο με αυτό το όνομα.'); location.href = 'project.php?id=".$id."'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+	$link->close();									//κλείσιμο σύνδεσης με βάση
+	echo "<script> alert('Υπάρχει αρχείο με αυτό το όνομα.'); location.href = 'project.php?id=".$id."'; </script>";
+											//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 }
 if ($ok) {
-	if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {														//απόπειρα μετακίνησης αρχείου στην διεύθυνση προορισμού
+	if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {		//απόπειρα μετακίνησης αρχείου στην διεύθυνση προορισμού
 		chmod($target_file, 0777);
 		$link->query ("INSERT INTO source_file (id, project_id, user, path, description) VALUES (DEFAULT, ".$id.", '".$_SESSION["session_lusername"]."', '".$target_file."', '".$_POST["description"]."')");
-																																//αν επιτυχία, εισαγωγή στον πίνακα source_file
+											//αν επιτυχία, εισαγωγή στον πίνακα source_file
 		echo mysqli_error($link);
 		$result->free();
-		$link->close();																											//κλείσιμο σύνδεσης με βάση
-		echo "<script> alert('Το αρχείο ανέβηκε.'); location.href = location.href = 'project.php?id=".$id."'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+		$link->close();								//κλείσιμο σύνδεσης με βάση
+		echo "<script> alert('Το αρχείο ανέβηκε.'); location.href = location.href = 'project.php?id=".$id."'; </script>";
+											//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 	}
-	else {																														//αν αποτυχία
+	else {										//αν αποτυχία
 		$result->free();
-		$link->close();																											//κλείσιμο σύνδεσης με βάση
-		echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";						//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+		$link->close();								//κλείσιμο σύνδεσης με βάση
+		echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";
+											//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 	}
 }
 ?>
