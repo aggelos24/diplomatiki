6fb0d9045cbc7e99576b9ad36c92a8b6e376b2e4
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 00:04:17 +0300

    Update delete_photo.php

--- code/learner/delete_photo.php
+++ code/learner/delete_photo.php
@@ -1,22 +1,22 @@
 <meta charset="utf-8" />
 <?php
-session_start();																						//δημιουργία συνεδρίας
-$link = mysqli_connect("localhost", "root", "", "diplomatiki"); 										//απόπειρα σύνδεσης στη βάση
-if (!$link) {																							//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";					//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+session_start();												//δημιουργία συνεδρίας
+$link = mysqli_connect("localhost", "root", "", "diplomatiki"); 						//απόπειρα σύνδεσης στη βάση
+if (!$link) {													//αν αποτυχία
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
-if (!(isset($_SESSION["session_lphoto"]))) {															//αν δεν υπάρχει φωτογραφία
-		$link->close();																					//κλείσιμο σύνδεσης με βάση
-		echo "<script> alert('Δεν υπάρχει φωτογραφία.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+if (!(isset($_SESSION["session_lphoto"]))) {									//αν δεν υπάρχει φωτογραφία
+		$link->close();											//κλείσιμο σύνδεσης με βάση
+		echo "<script> alert('Δεν υπάρχει φωτογραφία.'); location.href = 'lhome.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 }
-if (!unlink($_SESSION["session_lphoto"])) {																//απόπειρα διαγραφής εικόνας, αν αποτυχία
-		$link->close();																					//κλείσιμο σύνδεσης με βάση
-		echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+if (!unlink($_SESSION["session_lphoto"])) {									//απόπειρα διαγραφής εικόνας, αν αποτυχία
+		$link->close();											//κλείσιμο σύνδεσης με βάση
+		echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 }
 $link->query ("UPDATE user SET photo=NULL WHERE username='".$_SESSION["session_lusername"]."'");		//ενημέρωση του πίνακα user
-$_SESSION["session_lphoto"] = NULL;																		//ενημέρωση μεταβλητής συνεδρίας
-$link->close();																							//κλείσιμο σύνδεσης με βάση
-echo "<script> alert('Η εικόνα διαγράφηκε.'); location.href = 'lhome.php'; </script>";					//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+$_SESSION["session_lphoto"] = NULL;										//ενημέρωση μεταβλητής συνεδρίας
+$link->close();													//κλείσιμο σύνδεσης με βάση
+echo "<script> alert('Η εικόνα διαγράφηκε.'); location.href = 'lhome.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 ?>
