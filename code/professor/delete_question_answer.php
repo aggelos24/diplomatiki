e1c22fad37166419e797de4b21333e65bb2a2311
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 19:23:40 +0300

    Update delete_question_answer.php

--- code/professor/delete_question_answer.php
+++ code/professor/delete_question_answer.php
@@ -4,15 +4,15 @@ if (isset($_GET["id"])) {																			//αν υπάρχει η μεταβ�
 	$id = $_GET["id"];																				//ανάθεσή της σε μεταβλητή
 }
 else {																								//αν δεν υπάρχει
-	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
 }
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 									//απόπειρα σύνδεσης στη βάση
-if (!$link) {																						//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
+if (!$link) {												//αν αποτυχία
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
-$link->query ("DELETE FROM question_and_answer WHERE id=".$id);										//διαγραφή ερωτήματος από τον πίνακα question_and_answer
-$link->close();																						//κλείσιμο σύνδεσης με βάση
-echo "<script> alert('Το ερώτημα διαγράφηκε.'); location.href = 'test.php'; </script>";				//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
+$link->query ("DELETE FROM question_and_answer WHERE id=".$id);						//διαγραφή ερωτήματος από τον πίνακα question_and_answer
+$link->close();												//κλείσιμο σύνδεσης με βάση
+echo "<script> alert('Το ερώτημα διαγράφηκε.'); location.href = 'test.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
 ?>
