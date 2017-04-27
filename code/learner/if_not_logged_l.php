c2946fe6731ea491f88bc76d15b295483b6130f7
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 19:35:44 +0300

    Update if_not_logged_l.php

--- code/learner/if_not_logged_l.php
+++ code/learner/if_not_logged_l.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <?php
-session_start();														//δημιουργία συνεδρίας
-if (!(isset($_SESSION["session_lusername"]))) {											//αν δεν έχει συνδεθεί μαθητής
-	echo "<script> alert('Δεν έχεις συνδεθεί.'); location.href = '../login_register_form.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στη σελίδα login_register_form.php
+session_start();													//δημιουργία συνεδρίας
+if (!(isset($_SESSION["session_lusername"]))) {										//αν δεν έχει συνδεθεί μαθητής
+	echo "<script> alert('Δεν έχεις συνδεθεί.'); location.href = '../login_register_form.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στη σελίδα login_register_form.php
 }
 ?>
