b0bc5f9a07ebf96cffee536b1e968dd9d7dc3713
aggelos24 <angelos.arnaoutis@hotmail.com>
2017-04-27 13:20:46 +0300

    Update login.php

--- code/login.php
+++ code/login.php
@@ -3,13 +3,13 @@
 session_start();																							//δημιουργία συνεδρίας
 $link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 											//απόπειρα σύνδεσης στη βάση
 if (!$link) {																								//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'login_register_form.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα login_register_form.php
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'login_register_form.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα login_register_form.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT * FROM user");																//ανάκτηση των στοιχείων των χρηστών
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {													//για κάθε χρήστη
-	if ( ($row["username"] == $_POST["username"]) and ($row["password"] == md5($_POST["password"])) ) {		//αν ταιριάζουν τα στοιχεία της φόρμας με τα στοιχεία της βάσης
+	if ( ($row["username"] == $_POST["username"]) and ($row["password"] == md5($_POST["password"])) ) {	//αν ταιριάζουν τα στοιχεία της φόρμας με τα στοιχεία της βάσης
 		if ($row["professor"] == 1) {																		//αν είναι ο χρήστης ο εκπαιδευτικός
 			$_SESSION["session_pusername"] = "aggelos24";													//ορισμός μεταβλητών συνεδρίας
 			$result->free();
@@ -22,7 +22,8 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {													//για 
 			$_SESSION["session_lemail"] = $row["email"];
 			$_SESSION["session_ldescription"] = $row["description"];
 			$_SESSION["session_llevel"] = $row["level"];
-			$link->query ("UPDATE user SET last_login=NOW() WHERE username='".$_POST["username"]."'");	//ανανέωση της ημερομηνίας τελευταίας εισόδου του χρήστη
+			$link->query ("UPDATE user SET last_login=NOW() WHERE username='".$_POST["username"]."'");
+														//ανανέωση της ημερομηνίας τελευταίας εισόδου του χρήστη
 			$result->free();
 			$link->close();																					//κλείσιμο σύνδεσης με βάση
 			header("Location: learner/lhome.php");															//ανακατεύθυνση στο lhome.php
@@ -31,5 +32,5 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {													//για 
 }
 $result->free();
 $link->close();																								//κλείσιμο σύνδεσης με βάση
-echo "<script> alert('Λάθος στοιχεία.'); location.href = 'login_register_form.php'; </script>";				//αν δεν είναι χρήστης, εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα login_register_form.php
+echo "<script> alert('Λάθος στοιχεία.'); location.href = 'login_register_form.php'; </script>";			//αν δεν είναι χρήστης, εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα login_register_form.php
 ?>
