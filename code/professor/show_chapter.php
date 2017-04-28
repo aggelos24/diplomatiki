8d8cd958e317661873e1402a6717699a85d06cb6
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-04-28 13:34:23 +0300

    Update show_chapter.php

--- code/professor/show_chapter.php
+++ code/professor/show_chapter.php
@@ -7,7 +7,7 @@
 	<meta charset="utf-8" />
 	<title> Προβολή Κεφαλαίου </title>
 	<script>
-		function logout() {						//με το πάτημα του κουμπιού αποσύνδεση χρήστη
+		function logout() {								//με το πάτημα του κουμπιού αποσύνδεση χρήστη
 			location.href = "logout.php";
 		}
 	</script>
@@ -24,30 +24,28 @@
 	</div>
 	<div class="main">
 <?php
-include "if_not_logged_p.php";							//έλεγχος αν έχει συνδεθεί ο καθηγητής
-if ((isset($_GET["chapter"])) and (isset($_GET["section"]))) {			//αν υπάρχουν οι μεταβλητές GET
-	$chapter = $_GET["chapter"];						//ανάθεση σε μεταβλητές
+include "if_not_logged_p.php";									//έλεγχος αν έχει συνδεθεί ο καθηγητής
+if ((isset($_GET["chapter"])) and (isset($_GET["section"]))) {					//αν υπάρχουν οι μεταβλητές GET
+	$chapter = $_GET["chapter"];								//ανάθεση σε μεταβλητές
 	$section = $_GET["section"];
 }
-else {										//αν δεν υπάρχουν
-	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";
-										//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
+else {												//αν δεν υπάρχουν
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
 }
 echo "Προβολή | <a href='modify_chapter_form.php?section=".$section."&chapter=".$chapter."'> Επεξεργασία </a> <br> <br>";
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 		//απόπειρα σύνδεσης στη βάση
-if (!$link) {									//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";
-										//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 				//απόπειρα σύνδεσης στη βάση
+if (!$link) {											//αν αποτυχία
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT * FROM chapter WHERE section_number=".$section." AND number=".$chapter);
-										//ανάκτηση στοιχείων κεφαλαίου από τον πίνακα chapter
+												//ανάκτηση στοιχείων κεφαλαίου από τον πίνακα chapter
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-if (!empty($row)) {								//αν υπάρχει το κεφάλαιο
-	echo $row["section_number"].".".$row["number"]." ".$row["title"];	//εμφάνιση του κεφαλαίου
+if (!empty($row)) {										//αν υπάρχει το κεφάλαιο
+	echo $row["section_number"].".".$row["number"]." ".$row["title"];			//εμφάνιση του κεφαλαίου
 	echo "<hr> <br>";
-	$text = "<p class='text'>".$row["text"]."</p>";				//προσθήκη html tags για να φαίνεται όπως πρέπει στην σελίδα
+	$text = "<p class='text'>".$row["text"]."</p>";						//προσθήκη html tags για να φαίνεται όπως πρέπει στην σελίδα
 	$text = str_replace("\n", "\n</p> <p class='text'>", $text);
 	if (($row["image"] == NULL) and ($row["youtube"] == NULL)) {
 		echo $text;
@@ -68,13 +66,12 @@ if (!empty($row)) {								//αν υπάρχει το κεφάλαιο
 		}
 	}
 	$result->free();
-	$link->close();								//κλείσιμο σύνδεσης με βάση
+	$link->close();										//κλείσιμο σύνδεσης με βάση
 }
-else {										//αν δεν υπάρχει το κεφάλαιο
+else {												//αν δεν υπάρχει το κεφάλαιο
 	$result->free();
-	$link->close();								//κλείσιμο σύνδεσης με βάση
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";
-										//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
+	$link->close();										//κλείσιμο σύνδεσης με βάση
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
 }
 ?>
 		<br> <br> <a href="content.php"> Επιστροφή </a>
