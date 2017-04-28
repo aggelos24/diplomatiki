ac0fee676f0820ac11e3c220704a8e12169cba69
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-04-28 15:05:52 +0300

    Update show_notification_message.php

--- code/learner/show_notification_message.php
+++ code/learner/show_notification_message.php
@@ -1,17 +1,17 @@
 <?php
-session_start();												//δημιουργία συνεδρίας
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 						//απόπειρα σύνδεσης στη βάση
-if (!$link) {													//αν αποτυχία
-	echo "Κάτι πήγε στραβά";										//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+session_start();										//δημιουργία συνεδρίας
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 				//απόπειρα σύνδεσης στη βάση
+if (!$link) {											//αν αποτυχία
+	echo "Κάτι πήγε στραβά";								//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result=$link->query ("SELECT count(*) AS unseen FROM notification WHERE to_user='".$_SESSION["session_lusername"]."' AND seen=0 GROUP BY to_user");
-														//ανάκτηση αριθμού αδιάβαστων ειδοποιήσεων από τον πίνακα notification
+												//ανάκτηση αριθμού αδιάβαστων ειδοποιήσεων από τον πίνακα notification
 $notification=0;
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-if ($row["unseen"] == 1) {											//αν υπάρχουν
-	echo ", έχεις <span class='red_letters'>1</span> νέα ειδοποίηση";					//εμφάνιση κατάλληλων μηνυμάτων
+if ($row["unseen"] == 1) {									//αν υπάρχουν
+	echo ", έχεις <span class='red_letters'>1</span> νέα ειδοποίηση";			//εμφάνιση κατάλληλων μηνυμάτων
 	$notification = 1;
 }
 else if ($row["unseen"] > 1) {
@@ -19,30 +19,30 @@ else if ($row["unseen"] > 1) {
 	$notification = 1;
 }
 $result = $link->query ("SELECT count(*) AS unseen FROM message WHERE to_user='".$_SESSION["session_lusername"]."' AND seen=0 GROUP BY to_user");
-														//ανάκτηση αριθμού αδιάβαστων μηνυμάτων από τον πίνακα message
+												//ανάκτηση αριθμού αδιάβαστων μηνυμάτων από τον πίνακα message
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-if ($notification) {												//αν υπάρχουν ειδοποιήσεις
-	if ($row["unseen"] == 1) {										//αν υπάρχουν μηνύματα
-		echo ", και <span class='red_letters'> 1 </span> νέο μήνυμα";					//εμφάνιση κατάλληλων μηνυμάτων
+if ($notification) {										//αν υπάρχουν ειδοποιήσεις
+	if ($row["unseen"] == 1) {								//αν υπάρχουν μηνύματα
+		echo ", και <span class='red_letters'> 1 </span> νέο μήνυμα";			//εμφάνιση κατάλληλων μηνυμάτων
 	}
 	else if ($row["unseen"] > 1) {
 		echo ", και <span class='red_letters'>".$row["unseen"]."</span> νέα μηνύματα";
 	}
 }
-else {														//αν δεν υπάρχουν ειδοποιήσεις
-	if ($row["unseen"] == 1) {										//αν υπάρχουν μηνύματα
-		echo ", έχεις <span class='red_letters'>1</span> νέο μήνυμα";					//εμφάνιση κατάλληλων μηνυμάτων
+else {												//αν δεν υπάρχουν ειδοποιήσεις
+	if ($row["unseen"] == 1) {								//αν υπάρχουν μηνύματα
+		echo ", έχεις <span class='red_letters'>1</span> νέο μήνυμα";			//εμφάνιση κατάλληλων μηνυμάτων
 	}
 	else if ($row["unseen"] > 1) {
 		echo ", έχεις <span class='red_letters'>".$row["unseen"]."</span> νέα μηνύματα";
 	}
 }
 $result = $link->query ("SELECT count(*) AS test_num FROM test WHERE user='".$_SESSION["session_lusername"]."' AND test_date=CURDATE() AND status='pending'");
-														//ανάκτηση αριθμού εκκρεμών τεστ για το χρήστη με σημερινή ημερομηνία από τον πίνακα test
+												//ανάκτηση αριθμού εκκρεμών τεστ για το χρήστη με σημερινή ημερομηνία από τον πίνακα test
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-if ($row["test_num"] != 0) {											//αν υπάρχει εκκρεμές τεστ για το χρήστη με σημερινή ημερομηνία
+if ($row["test_num"] != 0) {									//αν υπάρχει εκκρεμές τεστ για το χρήστη με σημερινή ημερομηνία
 	echo "<br> <span class='red_letters'> Γράφεις τεστ σήμερα </span>";
 }
 $result->free();
-$link->close();													//κλείσιμο σύνδεσης με βάση
+$link->close();											//κλείσιμο σύνδεσης με βάση
 ?>
