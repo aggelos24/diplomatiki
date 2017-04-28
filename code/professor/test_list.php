788ad12a0137706e64eaad0b55c6b193d24f1c65
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-04-28 14:06:24 +0300

    Update test_list.php

--- code/professor/test_list.php
+++ code/professor/test_list.php
@@ -7,11 +7,11 @@
 	<meta charset="utf-8" />
 	<title> Λίστα Τεστ </title>
 	<script>
-		function logout() {																						//με το πάτημα του κουμπιού αποσύνδεση χρήστη
+		function logout() {								//με το πάτημα του κουμπιού αποσύνδεση χρήστη
 			location.href = "logout.php";
 		}
 		
-		function delete_test(id) {																				//με το πάτημα του κουμπιού διαγραφή τεστ
+		function delete_test(id) {							//με το πάτημα του κουμπιού διαγραφή τεστ
 			location.href = "delete_test.php?id="+id;
 		}
 	</script>
@@ -31,36 +31,37 @@
 		<span class="red_letters"> Αν διαγραφεί κάποιο τεστ, δεν επηρεάζεται το επίπεδο του μαθητή </span> <br> <br>
 <?php
 $to_greek = array("pending" => "Εκκρεμεί", "completed" => "Ολοκληρωμένο", "overdue" => "Εκπρόθεσμο");
-include "if_not_logged_p.php";																					//έλεγχος αν έχει συνδεθεί ο καθηγητής
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 												//απόπειρα σύνδεσης στη βάση
-if (!$link) {																									//αν αποτυχία
-	echo "<script> alert('Κάτι πήγε στραβά.'); window.location = 'insert_test_form.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα phome.php
+include "if_not_logged_p.php";									//έλεγχος αν έχει συνδεθεί ο καθηγητής
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 				//απόπειρα σύνδεσης στη βάση
+if (!$link) {											//αν αποτυχία
+	echo "<script> alert('Κάτι πήγε στραβά.'); window.location = 'insert_test_form.php'; </script>";
+												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα phome.php
 }
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
-$result = $link->query ("SELECT id FROM test WHERE CURDATE()>test_date AND status='pending'");					//ανάκτηση id από πίνακα test που εκκρεμούν και είναι στο παρελθόν 
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {														//για κάθε τεστ
-	$link->query ("UPDATE test SET status='overdue' WHERE id=".$row["id"]);										//ενημέρωση ότι είναι εκπρόθεσμο στον πίνακα test
+$result = $link->query ("SELECT id FROM test WHERE CURDATE()>test_date AND status='pending'");	//ανάκτηση id από πίνακα test που εκκρεμούν και είναι στο παρελθόν 
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {					//για κάθε τεστ
+	$link->query ("UPDATE test SET status='overdue' WHERE id=".$row["id"]);			//ενημέρωση ότι είναι εκπρόθεσμο στον πίνακα test
 }
 $result = $link->query ("SELECT * FROM test ORDER BY test_date DESC");
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {														//για κάθε τεστ
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {					//για κάθε τεστ
 	$status = $row["status"];
-	echo "<b>".$to_greek["$status"]."</b> <br>";																//εμφάνιση πληροφοριών
+	echo "<b>".$to_greek["$status"]."</b> <br>";						//εμφάνιση πληροφοριών
 	echo $row["user"].", ".date("d-m-Y", strtotime($row["test_date"])).", ";
-	if ($row["section_number"] == NULL) {																		//αν το τεστ είναι εφ' όλης της ύλης
+	if ($row["section_number"] == NULL) {							//αν το τεστ είναι εφ' όλης της ύλης
 		echo "Εφ' όλης της ύλης";
 	}
-	else {																										//αν είναι συγκεκριμένης ενότητας
+	else {											//αν είναι συγκεκριμένης ενότητας
 		echo "Ενότητα ".$row["section_number"];
 	}
-	if ($status == "completed") {																				//αν το τεστ έχει ολοκληρωθεί
-		echo ", ".$row["score"]." βαθμοί";																		//εμφάνιση βαθμολογίας
+	if ($status == "completed") {								//αν το τεστ έχει ολοκληρωθεί
+		echo ", ".$row["score"]." βαθμοί";						//εμφάνιση βαθμολογίας
 	}
 	$id = $row["id"];
 	echo "<br> <button onclick='delete_test(".$id.")'> Διαγραφή </button> <br> <br>";
 }
 $result->free();
-$link->close();																									//κλείσιμο σύνδεσης με βάση
+$link->close();											//κλείσιμο σύνδεσης με βάση
 ?>
 	</div>
 </body>
