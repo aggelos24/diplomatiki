1741002b230b1ee0dc0f3c1e59244ef03fb071bb
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-05-01 12:49:28 +0300

    Update message.php

--- code/professor/message.php
+++ code/professor/message.php
@@ -52,16 +52,15 @@ $result = $link->query ("SELECT * FROM message WHERE to_user='aggelos24' ORDER B
 												//ανάκτηση 10 το πολύ εισερχόμενων μηνυμάτων καθηγητή από τον πίνακα message
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {					//για κάθε εισερχόμενο μήνυμα
 	if ($row["seen"] == 0) {								//αν δεν έχει διαβαστεί
-		echo "<a href='show_message.php?id=".$row["id"]."' class='link_to_page'> <div class='not_seen_message_box'>".
+		echo "<a href='show_message.php?id=".$row["id"]."' class='link_to_page'> <div class='not_seen_message_box'> <div class='container'>".
 		"<div class='message_info'>".$row["from_user"]."</div>"."<div class='message_info'>".$row["subject"]."</div>"."<div class='message_info'>".date("H:i:s | d-m-Y", strtotime($row["date"]))."</div>";
-												//εμφάνιση πληροφοριών μηνύματος
-		echo "</div> </a>";
+		echo "</div> </div> </a>";							//εμφάνιση πληροφοριών μηνύματος
 	}
 	else {											//αν έχει διαβαστεί
-		echo "<a href='show_message.php?id=".$row["id"]."' class='link_to_page'> <div class='message_box'>".
+		echo "<a href='show_message.php?id=".$row["id"]."' class='link_to_page'> <div class='message_box'> <div class='container'>".
 		"<div class='message_info'>".$row["from_user"]."</div>"."<div class='message_info'>".$row["subject"]."</div>"."<div class='message_info'>".date("H:i:s | d-m-Y", strtotime($row["date"]))."</div>";
-												//εμφάνιση πληροφοριών μηνύματος
-		echo "</div> </a>";
+		echo "</div> </div> </a>";							//εμφάνιση πληροφοριών μηνύματος
+
 	}
 }
 if ($pagenum < ceil($count/10)) {								//αν δεν είμαστε στην τελευταία σελίδα
