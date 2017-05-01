ae0447e98e6d20fdee7426c4eadbcb3be1f7bf05
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-05-01 12:28:35 +0300

    Update message.php

--- code/learner/message.php
+++ code/learner/message.php
@@ -88,12 +88,12 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {					//για κάθε
 	if ($row["seen"] == 0) {								//αν δεν έχει διαβαστεί
 		echo "<a href='show_message.php?id=".$row["id"]."' class='link_to_page'> <div class='not_seen_message_box'> <div class='container'>".
 		"<div class='message_info'>".$from_user."</div>"."<div class='message_info'>".$row["subject"]."</div>"."<div class='message_info'>".date("H:i:s | d-m-Y", strtotime($row["date"]))."</div>";
-		echo "</div> </div> </a>";
+		echo "</div> </div> </a>";							//εμφάνιση πληροφοριών μηνύματος
 	}
 	else {											//αν έχει διαβαστεί
 		echo "<a href='show_message.php?id=".$row["id"]."' class='link_to_page'> <div class='message_box'> <div class='container'>".
 		"<div class='message_info'>".$from_user."</div>"."<div class='message_info'>".$row["subject"]."</div>"."<div class='message_info'>".date("H:i:s | d-m-Y", strtotime($row["date"]))."</div>";
-		echo "</div> </div> </a>";
+		echo "</div> </div> </a>";							//εμφάνιση πληροφοριών μηνύματος
 	}
 }
 if ($pagenum < ceil($count/10)) {								//αν υπάρχουν μηνύματα σε άλλη σελίδα
