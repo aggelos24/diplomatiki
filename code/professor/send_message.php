409d7d549173c2210a9af3230aa920354a7b42c7
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 13:26:04 +0300

    Update send_message.php

--- code/professor/send_message.php
+++ code/professor/send_message.php
@@ -1,15 +1,11 @@
 <meta charset="utf-8" />
 <?php
+include "../connect_to_database.php";
+$link = connect_to_database("phome.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $search = array("'", '"');
 $replace = array("\'", '\"');
 $message_text = str_replace($search, $replace, $_POST["message_text"]);				//για αποφυγή σφάλματος βάσης
 session_start();										//δημιουργία συνεδρίας
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 				//απόπειρα σύνδεσης στη βάση
-if (!$link) {											//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'phome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα phome.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT username FROM user where username='".$_POST["to_user"]."'");	//έλεγχος αν το όνομα χρήστη υπάρχει ήδη στη βάση
 if (empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {						//αν υπάρχει
 	$result->free();
