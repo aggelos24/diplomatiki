ab89e58ff23e5bcd499c5ab80cd08fb731ee4fa0
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 12:05:22 +0300

    Update not_display_notification.php

--- code/learner/not_display_notification.php
+++ code/learner/not_display_notification.php
@@ -1,18 +1,14 @@
 <meta charset="utf-8" />
 <?php
 session_start();											//δημιουργία συνεδρίας
+include "../connect_to_database.php";
+$link = connect_to_database("notification.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if ((isset($_GET["id"])) and (isset($_GET["id"]))) {							//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν όχι
 echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
 }
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $link->query ("UPDATE notification SET display=0 WHERE id=".$id);					//ενημέρωση του πίνακα notification
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Η ειδοποίηση δεν θα εμφανιστεί ξανά.'); location.href = 'notification.php'; </script>";
