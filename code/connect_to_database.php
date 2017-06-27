c21c98c9f999f43b40ffd272e303344aa2c725ec
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 11:34:18 +0300

    Add files via upload

---
+++ code/connect_to_database.php
@@ -0,0 +1,10 @@
+<?php
+function connect_to_database($destination) {
+    $link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 				//απόπειρα σύνδεσης στη βάση
+    if (!$link) {											//αν αποτυχία
+        echo "<script> alert('Κάτι πήγε στραβά.'); location.href = '".$destination."'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+    }
+    $link->query ("SET CHARACTER SET utf8");
+    $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
+    return $link;
+}
