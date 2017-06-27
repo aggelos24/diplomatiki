06876f982753f6ca79119f86cc61adf4c9f43805
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 12:13:04 +0300

    Update reject_friend_request.php

--- code/learner/reject_friend_request.php
+++ code/learner/reject_friend_request.php
@@ -1,18 +1,14 @@
 <meta charset="utf-8" />
 <?php
+include "../connect_to_database.php";
+$link = connect_to_database("notification.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if ((isset($_GET["id"]))) {										//αν υπάρχει η μεταβλητή GET
 	$friendship_id = $_GET["id"];									//ανάθεσή της σε μεταβλητή
 }
 else {													//αν όχι
-echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
 }
 session_start();											//δημιουργία συνεδρίας
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $link->query ("UPDATE friend_request SET status='rejected' WHERE id=".$friendship_id);			//ενημέρωση του πίνακα friend_request
 $result=$link->query ("SELECT notification_id FROM friend_request WHERE id=".$friendship_id);		//ανάκτηση id ειδοποίησης από τον πίνακα notification
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
