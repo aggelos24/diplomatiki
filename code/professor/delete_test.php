bf409cbacddfdc5456f4a3fe975aeec2d87772a9
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 12:51:54 +0300

    Update delete_test.php

--- code/professor/delete_test.php
+++ code/professor/delete_test.php
@@ -1,17 +1,13 @@
 <meta charset="utf-8" />
 <?php
+include "../connect_to_database.php";
+$link = connect_to_database("test_list.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (isset($_GET["id"])) {										//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test_list.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test_list.php
 }
-$link = mysqli_connect("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test_list.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test_list.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT notification_id FROM test WHERE id=".$id);				//ανάκτηση notification_id από τον πίνακα test
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $notification_id = $row["notification_id"];								//ανάθεσή της σε μεταβλητή
