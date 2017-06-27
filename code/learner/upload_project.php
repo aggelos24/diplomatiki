3159b17f354b1b06a8e5baf0bd09874db5ef2666
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 14:08:49 +0300

    Update upload_project.php

--- code/learner/upload_project.php
+++ code/learner/upload_project.php
@@ -10,8 +10,14 @@ else {												//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";
 												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 }
-include "../connect_to_database.php";
-$link = connect_to_database("project.php?id=".$id);						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 				//απόπειρα σύνδεσης στη βάση
+if (!$link) {											//αν αποτυχία
+	$ok = 0;
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project.php?id=".$id."'; </script>";
+												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+}
+$link->query ("SET CHARACTER SET utf8");
+$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $file_type = pathinfo(basename($_FILES["project_document"]["name"]),PATHINFO_EXTENSION);	//εύρεση επέκτασης αρχείου
 $target_file = "../projects/project_".$id."/project.doc";					//ορισμός διεύθυνσης προορισμού του αρχείου
 if ($file_type != "doc") {									//αν ο τύπος αρχείου δεν είναι doc
