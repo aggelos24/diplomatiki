3d4c7a214bbf8fba9b0aaee5f6bddbef85bdef80
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 12:49:35 +0300

    Update delete_project.php

--- code/professor/delete_project.php
+++ code/professor/delete_project.php
@@ -1,17 +1,13 @@
 <meta charset="utf-8" />
 <?php
+include "../connect_to_database.php";
+$link = connect_to_database("project_list.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (isset($_GET["id"])) {											//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];											//ανάθεσή της σε μεταβλητή
 }
 else {														//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project_list.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project_list.php
 }
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 						//απόπειρα σύνδεσης στη βάση
-if (!$link) {													//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project_list.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project_list.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT document FROM project WHERE id=".$id);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 if ($row["document"]) {												//αν κάποιος έχει ανέβασει την εργασία
