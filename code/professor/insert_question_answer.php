a28214cdc7f63bc01ac8fe59b626a2cae32936de
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 13:09:25 +0300

    Update insert_question_answer.php

--- code/professor/insert_question_answer.php
+++ code/professor/insert_question_answer.php
@@ -1,14 +1,10 @@
 <meta charset="utf-8" />
 <?php
+include "../connect_to_database.php";
+$link = connect_to_database("test.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (!isset($_POST["difficult"])) {									//αν είναι κενό το πεδίο δυσκολίας
 	echo "<script> alert('Δεν επέλεξες δυσκολία.'); location.href = 'test.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
 }
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $result = $link->query ("SELECT * FROM section WHERE number=".$_POST["section_number"]);		//έλεγχος αν υπάρχει ενότητα με αυτό τον αριθμό στον πίνακα section
 if (empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {							//αν υπάρχει
 	$result->free();
