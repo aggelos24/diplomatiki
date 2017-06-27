307239f909488d23e518f6c8c2fe85c8a2b3d0b5
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 12:47:05 +0300

    Update content.php

--- code/professor/content.php
+++ code/professor/content.php
@@ -43,12 +43,8 @@
 	<div class="main">
 <?php
 include "if_not_logged_p.php";										//έλεγχος αν έχει συνδεθεί ο καθηγητής
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'phome.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα phome.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
+include "../connect_to_database.php";
+$link = connect_to_database("phome.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $result = $link->query ("SELECT * FROM section");							//ανάκτηση ενοτήτων από τον πίνακα section
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {						//για κάθε ενότητα
 	echo "<b>".$row["number"].". ".$row["title"]."</b> <br>";			 		//εμφάνιση τίτλου ενότητας
