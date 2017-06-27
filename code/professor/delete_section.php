7f3b04efb96b9227146deb95c18b1d4124c9c1a1
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-06-27 12:51:12 +0300

    Update delete_section.php

--- code/professor/delete_section.php
+++ code/professor/delete_section.php
@@ -1,11 +1,7 @@
 <meta charset="utf-8" />
 <?php
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
-}
-$link->query ("SET CHARACTER SET utf8");
-$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
+include "../connect_to_database.php";
+$link = connect_to_database("content.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $result = $link->query ("SELECT * FROM section where number=".$_POST["number"]);			//έλεγχος αν υπάρχει ενότητα με αυτόν τον αριθμό στον πίνακα section
 if (empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {							//αν δεν υπάρχει
 	$result->free();
