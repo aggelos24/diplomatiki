1d86ba69734af88426dea6cc1c3db74745090e12
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-25 20:50:55 +0300

    Update upload_photo.php

--- code/learner/upload_photo.php
+++ code/learner/upload_photo.php
@@ -1,12 +1,8 @@
 <meta charset="utf-8" />
 <?php
+include "../connect_to_database.php";
+$link = connect_to_database("lhome.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 session_start();											//δημιουργία συνεδρίας
-$ok = 1;
-$link = mysqli_connect ("localhost", "root", "", "diplomatiki"); 					//απόπειρα σύνδεσης στη βάση
-if (!$link) {												//αν αποτυχία
-	$ok = 0;
-	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
-}
 $link->query ("SET CHARACTER SET utf8");
 $link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
 $file_type = pathinfo(basename($_FILES["photo"]["name"]), PATHINFO_EXTENSION);				//εύρεση επέκτασης αρχείου
@@ -17,22 +13,21 @@ if($check == false) {											//αν αποτυχία, σημαίνει πως
 	$link->close();											//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Το αρχείο που ανέβασες δεν είναι εικόνα.'); location.href = 'lhome.php'; </script>";
 													//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+	exit();												//τερματισμός script
 }
-if ($ok) {
-	if (isset($_SESSION["session_lphoto"])) {							//αν υπάρχει φωτογραφία
-		unlink($_SESSION["session_lphoto"]);							//διαγραφή υπάρχουσας εικόνας
-	}
-	if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {				//απόπειρα μετακίνησης εικόνας στην διεύθυνση προορισμού
-		chmod($target_file, 0777);
-		$_SESSION["session_lphoto"]=$target_file;						//αν επιτυχία, ενημέρωση της μεταβλητής συνεδρίας
-		$link->query ("UPDATE user SET photo='".$target_file."' WHERE username='".$_SESSION["session_lusername"]."'");
+if (isset($_SESSION["session_lphoto"])) {								//αν υπάρχει φωτογραφία
+	unlink($_SESSION["session_lphoto"]);								//διαγραφή υπάρχουσας εικόνας
+}
+if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {					//απόπειρα μετακίνησης εικόνας στην διεύθυνση προορισμού
+	chmod($target_file, 0777);
+	$_SESSION["session_lphoto"]=$target_file;							//αν επιτυχία, ενημέρωση της μεταβλητής συνεδρίας
+	$link->query ("UPDATE user SET photo='".$target_file."' WHERE username='".$_SESSION["session_lusername"]."'");
 													//ενημέρωση του πίνακα user
-		$link->close();										//κλείσιμο σύνδεσης με βάση
-		echo "<script> alert('Η εικόνα ανέβηκε.'); location.href = 'lhome.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
-	}
-	else {												//αν αποτυχία
-		$link->close();										//κλείσιμο σύνδεσης με βάση
-		echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
-	}
+	$link->close();											//κλείσιμο σύνδεσης με βάση
+	echo "<script> alert('Η εικόνα ανέβηκε.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+}
+else {													//αν αποτυχία
+	$link->close();											//κλείσιμο σύνδεσης με βάση
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 }
 ?>
