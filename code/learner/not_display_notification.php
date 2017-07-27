1ef73a001986a8c4e17ea8ad2526ad4eadbd92e0
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-27 13:40:56 +0300

    Update not_display_notification.php

--- code/learner/not_display_notification.php
+++ code/learner/not_display_notification.php
@@ -7,9 +7,10 @@ if ((isset($_GET["id"])) and (isset($_GET["id"]))) {							//αν υπάρχει
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν όχι
-echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
+	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
+	exit();												//τερματισμός script
 }
-$link->query ("UPDATE notification SET display=0 WHERE id=".$id);					//ενημέρωση του πίνακα notification
+$link->query("UPDATE notification SET display=0 WHERE id=".$id);					//ενημέρωση του πίνακα notification
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Η ειδοποίηση δεν θα εμφανιστεί ξανά.'); location.href = 'notification.php'; </script>";
 													//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
