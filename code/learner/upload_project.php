31c7f2d6fd8604de3bebdfa5e63fe72c92e82439
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-27 14:28:42 +0300

    Update upload_project.php

--- code/learner/upload_project.php
+++ code/learner/upload_project.php
@@ -10,11 +10,9 @@ else {												//αν δεν υπάρχει
 												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 	exit();											//τερματισμός script
 }
-$link = connect_to_database("project.php?id=".$id);						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $file_type = pathinfo(basename($_FILES["project_document"]["name"]),PATHINFO_EXTENSION);	//εύρεση επέκτασης αρχείου
 $target_file = "../projects/project_".$id."/project.doc";					//ορισμός διεύθυνσης προορισμού του αρχείου
 if ($file_type != "doc") {									//αν ο τύπος αρχείου δεν είναι doc
-	$link->close();										//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Το αρχείο δεν είναι .doc.'); location.href = 'project.php?id=".$id."'; </script>";
 												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 	exit();											//τερματισμός script
@@ -22,6 +20,7 @@ if ($file_type != "doc") {									//αν ο τύπος αρχείου δεν ε
 if (file_exists($target_file)) {								//αν έχει ανεβεί ήδη η εργασία
 	unlink($target_file);									//διαγραφή προηγούμενης εργασίας
 }
+$link = connect_to_database("project.php?id=".$id);						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (move_uploaded_file($_FILES["project_document"]["tmp_name"], $target_file)) {		//απόπειρα μετακίνησης εικόνας στην διεύθυνση προορισμού
 	chmod($target_file, 0777);
 	$link->query ("UPDATE project SET document=1 WHERE id=".$id);				//αν επιτυχία, ενημέρωση του πίνακα project
