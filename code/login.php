3197744207797ed4e7b6cf316bd4333ca1286b5f
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-27 12:36:58 +0300

    Update login.php

--- code/login.php
+++ code/login.php
@@ -3,7 +3,7 @@
 session_start();												//δημιουργία συνεδρίας
 include "connect_to_database.php";
 $link = connect_to_database("login_register_form.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
-$result = $link->query ("SELECT * FROM user");									//ανάκτηση των στοιχείων των χρηστών
+$result = $link->query("SELECT * FROM user");									//ανάκτηση των στοιχείων των χρηστών
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {							//για κάθε χρήστη
 	if ( ($row["username"] == $_POST["username"]) and ($row["password"] == md5($_POST["password"])) ) {	//αν ταιριάζουν τα στοιχεία της φόρμας με τα στοιχεία της βάσης
 		if ($row["professor"] == 1) {									//αν είναι ο καθηγητής
@@ -18,7 +18,7 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {							//για κάθ
 			$_SESSION["session_lemail"] = $row["email"];
 			$_SESSION["session_ldescription"] = $row["description"];
 			$_SESSION["session_llevel"] = $row["level"];
-			$link->query ("UPDATE user SET last_login=NOW() WHERE username='".$_POST["username"]."'");
+			$link->query("UPDATE user SET last_login=NOW() WHERE username='".$_POST["username"]."'");
 														//ανανέωση της ημερομηνίας τελευταίας εισόδου του χρήστη
 			$result->free();
 			$link->close();										//κλείσιμο σύνδεσης με βάση
