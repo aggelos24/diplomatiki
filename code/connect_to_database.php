08927e80bfff155fa3ac4616365709641a95b2f8
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-27 12:35:47 +0300

    Update connect_to_database.php

--- code/connect_to_database.php
+++ code/connect_to_database.php
@@ -1,12 +1,12 @@
 <?php
 function connect_to_database($destination) {					//συνάρτηση για σύνδεση στη βάση
-	$link = mysqli_connect ("localhost", "root", "", "diplomatiki");		//απόπειρα σύνδεσης στη βάση
+	$link = mysqli_connect("localhost", "root", "", "diplomatiki");		//απόπειρα σύνδεσης στη βάση
 	if (!$link) {								//αν αποτυχία
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = '".$destination."'; </script>";
 										//εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στην κατάλληλη σελίδα
 	exit();									//τερματισμός script
 	}
-	$link->query ("SET CHARACTER SET utf8");
-	$link->query ("SET COLLATION_CONNECTION=utf8_general_ci");
+	$link->query("SET CHARACTER SET utf8");
+	$link->query("SET COLLATION_CONNECTION=utf8_general_ci");
 	return $link;
 }
