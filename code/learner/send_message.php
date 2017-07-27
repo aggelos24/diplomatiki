44e9f8677114d3256838a972b562340b11c3da54
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-27 14:00:05 +0300

    Update send_message.php

--- code/learner/send_message.php
+++ code/learner/send_message.php
@@ -1,15 +1,18 @@
 <meta charset="utf-8" />
 <?php
 include "../connect_to_database.php";
-$link = connect_to_database("message.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+session_start();										//δημιουργία συνεδρίας
 $search = array("'", '"');
 $replace = array("\'", '\"');
 $message_text = str_replace($search, $replace, $_POST["message_text"]);				//για αποφυγή σφάλματος βάσης
-session_start();										//δημιουργία συνεδρίας
 if ($_POST["to_user"] == "Καθηγητής") {								//αν ο παραλήπτης είναι ο καθηγητής
-	$_POST["to_user"] = "aggelos24";
+	$to_user = "aggelos24";
 }
-$link->query ("INSERT INTO message (id, from_user, to_user, subject, text, seen, date) VALUES (DEFAULT, '".$_SESSION["session_lusername"]."', '".$_POST["to_user"]."', '".$_POST["subject"]."', '".$message_text."', DEFAULT, NOW())");
+else {
+	$to_user = $_POST["to_user"];
+}
+$link = connect_to_database("message.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link->query("INSERT INTO message (id, from_user, to_user, subject, text, seen, date) VALUES (DEFAULT, '".$_SESSION["session_lusername"]."', '".$to_user."', '".$_POST["subject"]."', '".$message_text."', DEFAULT, NOW())");
 												//δημιουργία μηνύματος στη βάση
 $link->close();											//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = 'message.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα message.php
