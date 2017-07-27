9210a4fb06fde4f21df59c7409fd1293cd37158d
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-27 13:14:35 +0300

    Update delete_photo.php

--- code/learner/delete_photo.php
+++ code/learner/delete_photo.php
@@ -1,18 +1,16 @@
 <meta charset="utf-8" />
 <?php
 include "../connect_to_database.php";
-$link = connect_to_database("lhome.php");									//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 session_start();												//δημιουργία συνεδρίας
 if (!(isset($_SESSION["session_lphoto"]))) {									//αν δεν υπάρχει φωτογραφία
-	$link->close();												//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Δεν υπάρχει φωτογραφία.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 	exit();													//τερματισμός script
 }
 if (!unlink($_SESSION["session_lphoto"])) {									//απόπειρα διαγραφής εικόνας, αν αποτυχία
-	$link->close();												//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
 	exit();													//τερματισμός script
 }
+$link = connect_to_database("lhome.php");									//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $link->query("UPDATE user SET photo=NULL WHERE username='".$_SESSION["session_lusername"]."'");			//ενημέρωση του πίνακα user
 $_SESSION["session_lphoto"] = NULL;										//ενημέρωση μεταβλητής συνεδρίας
 $link->close();													//κλείσιμο σύνδεσης με βάση
