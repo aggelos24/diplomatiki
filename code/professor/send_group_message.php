be445fd000152880ac3f4338c9810933929edaa0
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-30 18:41:42 +0300

    Update send_group_message.php

--- code/professor/send_group_message.php
+++ code/professor/send_group_message.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8" />
 <?php
+$professor_username = "aggelos24";										//ανάθεση του username του καθηγητή σε μεταβλητή
 $search = array("'", '"');
 $replace = array("\'", '\"');
 $message_text = str_replace($search, $replace, $_POST["message_text"]);						//για αποφυγή σφάλματος βάσης
@@ -11,9 +12,8 @@ else {														//αν δεν υπάρχει
 }
 include "../connect_to_database.php";
 $link = connect_to_database("project.php?id=".$id);								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
-$link->query ("INSERT INTO group_chat (id, project_id, user, text) VALUES (DEFAULT, ".$id.", 'aggelos24', '".$message_text."')");
+$link->query ("INSERT INTO group_chat (id, project_id, user, text) VALUES (DEFAULT, ".$id.", '".$professor_username."', '".$message_text."')");
 														//εισαγωγή μηνύματος στον πίνακα group_chat
 $link->close();													//κλείσιμο σύνδεσης με βάση
-echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = 'project.php?id=".$id."'; </script>";
-														//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
+echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = 'project.php?id=".$id."'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 ?>
