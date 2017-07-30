2724657ea38b5bd1be60cb1687d9ca63e812675b
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-07-30 18:33:21 +0300

    Update if_not_logged_p.php

--- code/professor/if_not_logged_p.php
+++ code/professor/if_not_logged_p.php
@@ -1,8 +1,9 @@
 <meta charset="utf-8" />
 <?php
-session_start();						//δημιουργία συνεδρίας
-if ($_SESSION["session_pusername"] != "aggelos24") {		//αν δεν έχει συνδεθεί ο καθηγητής
+$professor_username = "aggelos24";					//ανάθεση του username του καθηγητή σε μεταβλητή
+session_start();							//δημιουργία συνεδρίας
+if ($_SESSION["session_pusername"] != $professor_username) {		//αν δεν έχει συνδεθεί ο καθηγητής
 	echo "<script> alert('Δεν έχεις συνδεθεί.'); location.href = '../login_register_form.php'; </script>";
-								//εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στη σελίδα login_register_form.php
+									//εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στη σελίδα login_register_form.php
 }
 ?>
