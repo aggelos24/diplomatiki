ba115efe3ee488d56acd7102a4d7cdb69c5c4968
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:23:32 +0300

    Update grade_project.php

--- code/professor/grade_project.php
+++ code/professor/grade_project.php
@@ -1,14 +1,15 @@
 <meta charset="utf-8" />
 <?php
 include "../connect_to_database.php";
-$link = connect_to_database("project_list.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (isset($_GET["id"])) {										//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project_list.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project_list.php
+	exit();												//τερματισμός script
 }
-$link->query ("UPDATE project SET grade=".$_POST["grade"]." WHERE id=".$id);				//ενημέρωση πίνακα project με τον καινούργιο βαθμό	
+$link = connect_to_database("project_list.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link->query("UPDATE project SET grade=".$_POST["grade"]." WHERE id=".$id);				//ενημέρωση πίνακα project με τον καινούργιο βαθμό	
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Η εργασία βαθμολογήθηκε, μπορείς με την ίδια διαδικασία να αλλάξεις τον βαθμό.'); location.href = 'project_list.php'; </script>";
 													//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project_list.php
