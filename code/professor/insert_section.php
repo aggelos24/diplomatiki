4491ff576e007864c255b8a9a5aefa84b90dec29
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:41:49 +0300

    Update insert_section.php

--- code/professor/insert_section.php
+++ code/professor/insert_section.php
@@ -2,15 +2,16 @@
 <?php
 include "../connect_to_database.php";
 $link = connect_to_database("content.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
-$result = $link->query ("SELECT * FROM section where number=".$_POST["number"]);			//έλεγχος αν υπάρχει ενότητα με αυτόν τον αριθμό
+$result = $link->query("SELECT * FROM section where number=".$_POST["number"]);				//έλεγχος αν υπάρχει ενότητα με αυτόν τον αριθμό
 if (!empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {						//αν υπάρχει
 	$result->free();
 	$link->close();											//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Υπάρχει ήδη ενότητα με αυτό τον αριθμό.'); location.href = 'content.php'; </script>";
 													//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
+	exit();												//τερματισμός script
 }
 else {													//αν δεν υπάρχει
-	$link->query ("INSERT INTO section (number, title) VALUES (".$_POST["number"].", '".$_POST["title"]."')");
+	$link->query("INSERT INTO section (number, title) VALUES (".$_POST["number"].", '".$_POST["title"]."')");
 													//εισαγωγή ενότητας στον πίνακα section
 	$result->free();
 	$link->close();											//κλείσιμο σύνδεσης με βάση
