6c426be508faeee81f1edbfa39100697b15e2140
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:16:34 +0300

    Update delete_question_answer.php

--- code/professor/delete_question_answer.php
+++ code/professor/delete_question_answer.php
@@ -1,14 +1,15 @@
 <meta charset="utf-8" />
 <?php
 include "../connect_to_database.php";
-$link = connect_to_database("test.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (isset($_GET["id"])) {										//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
+	exit();												//τερματισμός script
 }
-$link->query ("DELETE FROM question_and_answer WHERE id=".$id);						//διαγραφή ερωτήματος από τον πίνακα question_and_answer
+$link = connect_to_database("test.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link->query("DELETE FROM question_and_answer WHERE id=".$id);						//διαγραφή ερωτήματος από τον πίνακα question_and_answer
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Το ερώτημα διαγράφηκε.'); location.href = 'test.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
 ?>
