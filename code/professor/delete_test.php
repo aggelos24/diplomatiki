129259a3ff7892729a4794d6649ccc31bfddfc25
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:18:22 +0300

    Update delete_test.php

--- code/professor/delete_test.php
+++ code/professor/delete_test.php
@@ -1,17 +1,18 @@
 <meta charset="utf-8" />
 <?php
 include "../connect_to_database.php";
-$link = connect_to_database("test_list.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (isset($_GET["id"])) {										//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test_list.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test_list.php
+	exit();												//τερματισμός script
 }
-$result = $link->query ("SELECT notification_id FROM test WHERE id=".$id);				//ανάκτηση notification_id από τον πίνακα test
+$link = connect_to_database("test_list.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$result = $link->query("SELECT notification_id FROM test WHERE id=".$id);				//ανάκτηση notification_id από τον πίνακα test
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $notification_id = $row["notification_id"];								//ανάθεσή της σε μεταβλητή
-$link->query ("DELETE FROM notification WHERE id=".$notification_id);					//διαγραφή ειδοποίησης και τεστ από τους αντίστοιχους πίνακες
+$link->query("DELETE FROM notification WHERE id=".$notification_id);					//διαγραφή ειδοποίησης και τεστ από τους αντίστοιχους πίνακες
 $result->free();
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Το τεστ διαγράφηκε.'); location.href = 'test_list.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test_list.php
