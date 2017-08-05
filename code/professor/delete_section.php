31bf5eab4532552ec033ee3dbaa8302424a693fc
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:17:32 +0300

    Update delete_section.php

--- code/professor/delete_section.php
+++ code/professor/delete_section.php
@@ -2,15 +2,16 @@
 <?php
 include "../connect_to_database.php";
 $link = connect_to_database("content.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
-$result = $link->query ("SELECT * FROM section where number=".$_POST["number"]);			//έλεγχος αν υπάρχει ενότητα με αυτόν τον αριθμό στον πίνακα section
+$result = $link->query("SELECT * FROM section where number=".$_POST["number"]);				//έλεγχος αν υπάρχει ενότητα με αυτόν τον αριθμό στον πίνακα section
 if (empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {							//αν δεν υπάρχει
 	$result->free();
 	$link->close();											//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Δεν υπάρχει ενότητα με αυτόν τον αριθμό.'); location.href ='content.php'; </script>";
 													//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
+	exit();												//τερματισμός script
 }
 else {													//αν υπάρχει
-	$link->query ("DELETE FROM section WHERE number=".$_POST["number"]);				//διαγραφή ενότητας από τον πίνακα section
+	$link->query("DELETE FROM section WHERE number=".$_POST["number"]);				//διαγραφή ενότητας από τον πίνακα section
 	$result->free();
 	$link->close();											//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Η ενότητα διαγράφηκε.'); location.href = 'content.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
