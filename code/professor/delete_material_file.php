a406b9dc6f58d2339470b421d9712908c8baf187
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:09:43 +0300

    Update delete_material_file.php

--- code/professor/delete_material_file.php
+++ code/professor/delete_material_file.php
@@ -1,18 +1,19 @@
 <meta charset="utf-8" />
 <?php
 include "../connect_to_database.php";
-$link = connect_to_database("material.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (isset($_GET["path"])) {										//αν υπάρχει η μεταβλητή GET
 	$path = $_GET["path"];										//ανάθεσή της σε μεταβλητή
 }
 else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'material.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα material.php
+	exit();												//τερματισμός script
 }
 if (!unlink($path)) {											//απόπειρα διαγραφής αρχείου, αν αποτυχία
-	$link->close();											//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'material.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα material.php
+	exit();												//τερματισμός script
 }
-$link->query ("DELETE FROM material WHERE path='".$path."'");						//διαγραφή εγγραφής από τον πίνακα material
+$link = connect_to_database("material.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link->query("DELETE FROM material WHERE path='".$path."'");						//διαγραφή εγγραφής από τον πίνακα material
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Το αρχείο διαγράφηκε.'); location.href = 'material.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα material.php
 ?>
