da8c41550ad613b7ba9ef9ddc6e70b5ce9976152
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-08-05 12:21:02 +0300

    Update get_learner_list.php

--- code/professor/get_learner_list.php
+++ code/professor/get_learner_list.php
@@ -5,17 +5,16 @@ if ((isset($_GET["sort"]))) {							//αν υπάρχει η μεταβλητή 
 	$sort = $_GET["sort"];							//ανάθεσή της σε μεταβλητή
 }
 if ($sort == "name") {								//αν δεν έχει οριστεί τύπος ταξινόμησης
-	$result = $link->query ("SELECT * FROM user WHERE professor=0 ORDER BY username");
+	$result = $link->query("SELECT * FROM user WHERE professor=0 ORDER BY username");
 										//ανάκτηση στοιχείων μαθητών από τον πίνακα user
 }
 else if ($sort == "level") {								//αν η ταξινόμηση είναι με βάση το επίπεδο
-	$result = $link->query ("SELECT * FROM user WHERE professor=0 ORDER BY level");										//ανάκτηση στοιχείων μαθητών ταξινομημένα με βάση το επίπεδο από τον πίνακα user
+	$result = $link->query("SELECT * FROM user WHERE professor=0 ORDER BY level");										//ανάκτηση στοιχείων μαθητών ταξινομημένα με βάση το επίπεδο από τον πίνακα user
 }
 else {										//αν η ταξινόμηση είναι με βάση την ημερομηνία τελευταίας σύνδεσης χρήστη
-	$result = $link->query ("SELECT * FROM user WHERE professor=0 ORDER BY last_login DESC");
+	$result = $link->query("SELECT * FROM user WHERE professor=0 ORDER BY last_login DESC");
 										//ανάκτηση στοιχείων μαθητών ταξινομημένα με βάση την ημερομηνία τελευταίας σύνδεσης χρήστη από τον πίνακα user
 }
-
 $i=0;
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {			//για κάθε μαθητή
     $results[$i] = [ "username" => $row["username"], "email" => $row["email"], "level" => $row["level"], "last_login" => date("d-m-Y", strtotime($row["last_login"]))];
