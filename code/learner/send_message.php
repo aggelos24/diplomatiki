7a3a3d87578785dc4f12f328fcfe1f2f69f85cc3
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-22 21:56:16 +0300

    Update send_message.php

--- code/learner/send_message.php
+++ code/learner/send_message.php
@@ -6,15 +6,22 @@ session_start();										//δημιουργία συνεδρίας
 $search = array("'", '"');
 $replace = array("\'", '\"');
 $message_text = str_replace($search, $replace, $_POST["message_text"]);				//για αποφυγή σφάλματος βάσης
+if (($_GET["from"] == "lhome") or ($_GET["from"] == "message")) {				//αν η μεταβλητή GET έχει κάποια τις δύο τιμές
+    $from = $_GET["from"].".php";								//ανάθεσή της σε μεταβλητή
+}
+else {												//αν όχι
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'lhome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+    exit();											//τερματισμός script
+}
 if ($_POST["to_user"] == "Καθηγητής") {								//αν ο παραλήπτης είναι ο καθηγητής
 	$to_user = $professor_username;
 }
-else {
+else {												//αν όχι
 	$to_user = $_POST["to_user"];
 }
-$link = connect_to_database("message.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database($from);								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $link->query("INSERT INTO message (id, from_user, to_user, subject, text, seen, date) VALUES (DEFAULT, '".$_SESSION["session_lusername"]."', '".$to_user."', '".$_POST["subject"]."', '".$message_text."', DEFAULT, NOW())");
 												//δημιουργία μηνύματος στη βάση
 $link->close();											//κλείσιμο σύνδεσης με βάση
-echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = 'message.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα message.php
+echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = '".$from."'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στην προηγούμενη σελίδα
 ?>
