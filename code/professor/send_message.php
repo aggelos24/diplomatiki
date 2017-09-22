60d00e317e5a540d186ff11bbf986f91cbddca21
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-22 22:33:10 +0300

    Update send_message.php

--- code/professor/send_message.php
+++ code/professor/send_message.php
@@ -5,19 +5,26 @@ $professor_username = "aggelos24";								//ανάθεση του username το�
 $search = array("'", '"');
 $replace = array("\'", '\"');
 $message_text = str_replace($search, $replace, $_POST["message_text"]);				//για αποφυγή σφάλματος βάσης
-$link = connect_to_database("phome.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+if (($_GET["from"] == "phome") or ($_GET["from"] == "message")) {				//αν η μεταβλητή GET έχει κάποια τις δύο τιμές
+    $from = $_GET["from"].".php";								//ανάθεσή της σε μεταβλητή
+}
+else {												//αν όχι
+    echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'phome.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
+    exit();											//τερματισμός script
+}
+$link = connect_to_database($from);								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $result = $link->query ("SELECT username FROM user where username='".$_POST["to_user"]."'");	//έλεγχος αν το όνομα χρήστη υπάρχει ήδη στη βάση
 if (empty(mysqli_fetch_array($result, MYSQLI_ASSOC))) {						//αν υπάρχει
 	$result->free();
 	$link->close();										//κλείσιμο σύνδεσης με βάση
-	echo "<script> alert('Δεν υπάρχει μαθητής με αυτό το Όνομα Χρήστη.'); location.href = 'phome.php'; </script>";
-												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα phome.php
+	echo "<script> alert('Δεν υπάρχει μαθητής με αυτό το Όνομα Χρήστη.'); location.href = '".$from."'; </script>";
+												//εμφάνιση κατάλληλου μηνύματος και επιστροφή στην προηγούμενη σελίδα
 }
 else {
 	$link->query ("INSERT INTO message (id, from_user, to_user, subject, text, seen, date) VALUES (DEFAULT, '".$professor_username."', '".$_POST["to_user"]."', '".$_POST["subject"]."', '".$message_text."', DEFAULT, NOW())");
 												//δημιουργία μηνύματος στη βάση
 	$result->free();
 	$link->close();										//κλείσιμο σύνδεσης με βάση
-	echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = 'phome.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα phome.php
+	echo "<script> alert('Το μήνυμα στάλθηκε.'); location.href = '".$from."'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στην προηγούμενη σελίδα
 }
 ?>
