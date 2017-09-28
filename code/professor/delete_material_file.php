1d0466e58c34f404c60d3697ae2121744de0f66f
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 21:12:52 +0300

    Update delete_material_file.php

--- code/professor/delete_material_file.php
+++ code/professor/delete_material_file.php
@@ -12,7 +12,7 @@ if (!unlink($path)) {											//απόπειρα διαγραφής αρχεί
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'material.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα material.php
 	exit();												//τερματισμός script
 }
-$link = connect_to_database("material.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database("../login_register_form.php");						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $link->query("DELETE FROM material WHERE path='".$path."'");						//διαγραφή εγγραφής από τον πίνακα material
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Το αρχείο διαγράφηκε.'); location.href = 'material.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα material.php
