53fc4788e843105879a62e960ee0e0f5e95d806a
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 21:15:37 +0300

    Update grade_project.php

--- code/professor/grade_project.php
+++ code/professor/grade_project.php
@@ -8,7 +8,7 @@ else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'project_list.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project_list.php
 	exit();												//τερματισμός script
 }
-$link = connect_to_database("project_list.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database("../login_register_form.php");						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $link->query("UPDATE project SET grade=".$_POST["grade"]." WHERE id=".$id);				//ενημέρωση πίνακα project με τον καινούργιο βαθμό	
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Η εργασία βαθμολογήθηκε, μπορείς με την ίδια διαδικασία να αλλάξεις τον βαθμό.'); location.href = 'project_list.php'; </script>";
