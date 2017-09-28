91ef757a134e615d66cb417185181acd2850c1ab
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 20:56:09 +0300

    Update not_display_notification.php

--- code/learner/not_display_notification.php
+++ code/learner/not_display_notification.php
@@ -2,7 +2,7 @@
 <?php
 session_start();											//δημιουργία συνεδρίας
 include "../connect_to_database.php";
-$link = connect_to_database("notification.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database("../login_register_form.php");						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if ((isset($_GET["id"])) and (isset($_GET["id"]))) {							//αν υπάρχει η μεταβλητή GET
 	$id = $_GET["id"];										//ανάθεσή της σε μεταβλητή
 }
