b5b8556741a3024b15687c0c5dc65a69a2d093e1
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 20:57:27 +0300

    Update reject_friend_request.php

--- code/learner/reject_friend_request.php
+++ code/learner/reject_friend_request.php
@@ -9,7 +9,7 @@ else {													//αν όχι
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'notification.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα notification.php
 	exit();												//τερματισμός script
 }
-$link = connect_to_database("notification.php");							//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database("../login_register_form.php");						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $link->query("UPDATE friend_request SET status='rejected' WHERE id=".$friendship_id);			//ενημέρωση του πίνακα friend_request
 $result=$link->query("SELECT notification_id FROM friend_request WHERE id=".$friendship_id);		//ανάκτηση id ειδοποίησης από τον πίνακα notification
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
