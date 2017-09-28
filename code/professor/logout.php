160a1cb54550f1a0d01afdef57329a8d9c641adf
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 20:22:33 +0300

    Update logout.php

--- code/professor/logout.php
+++ code/professor/logout.php
@@ -2,7 +2,7 @@
 <?php
 session_start();                                                      //δημιουργία συνεδρίας
 session_unset();                                                      //διαγραφή μεταβλητών συνεδρίας
-session_write_close();                                                //διαγραφή συνεδρίας
+session_destroy();                                                    //διαγραφή συνεδρίας
 echo "<script> alert('Αποσυνδέθηκες.'); location.href = '../login_register_form.php'; </script>";
                                                                       //εμφάνιση κατάλληλου μηνύματος και ανακατεύθυνση στη σελίδα login_register_form.php
 ?>
