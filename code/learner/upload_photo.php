e4852c0bffd31e02c269fc15a75f09e69f89dc51
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 21:03:47 +0300

    Update upload_photo.php

--- code/learner/upload_photo.php
+++ code/learner/upload_photo.php
@@ -5,7 +5,7 @@ session_start();											//δημιουργία συνεδρίας
 $file_type = pathinfo(basename($_FILES["photo"]["name"]), PATHINFO_EXTENSION);				//εύρεση επέκτασης αρχείου
 $target_file = "photos/".$_SESSION["session_lusername"].".".$file_type;					//ορισμός διεύθυνσης προορισμού του αρχείου
 $check = getimagesize($_FILES["photo"]["tmp_name"]);							//απόπειρα εύρεσης μεγέθους εικόνας
-if($check == false) {											//αν αποτυχία, σημαίνει πως δεν είναι εικόνα το αρχείο
+if ($check == false) {											//αν αποτυχία, σημαίνει πως δεν είναι εικόνα το αρχείο
 	$ok = 0;
 	echo "<script> alert('Το αρχείο που ανέβασες δεν είναι εικόνα.'); location.href = 'lhome.php'; </script>";
 													//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα lhome.php
@@ -14,7 +14,7 @@ if($check == false) {											//αν αποτυχία, σημαίνει πως
 if (isset($_SESSION["session_lphoto"])) {								//αν υπάρχει φωτογραφία
 	unlink($_SESSION["session_lphoto"]);								//διαγραφή υπάρχουσας εικόνας
 }
-$link = connect_to_database("lhome.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database("../login_register_form.php");						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {					//απόπειρα μετακίνησης εικόνας στην διεύθυνση προορισμού
 	chmod($target_file, 0777);
 	$_SESSION["session_lphoto"]=$target_file;							//αν επιτυχία, ενημέρωση της μεταβλητής συνεδρίας
