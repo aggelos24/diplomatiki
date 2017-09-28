6b91ac13b466ef4923c3bdf37ce5dbab233d86cf
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-09-28 21:13:33 +0300

    Update delete_question_answer.php

--- code/professor/delete_question_answer.php
+++ code/professor/delete_question_answer.php
@@ -8,7 +8,7 @@ else {													//αν δεν υπάρχει
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'test.php'; </script>";		//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
 	exit();												//τερματισμός script
 }
-$link = connect_to_database("test.php");								//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
+$link = connect_to_database("../login_register_form.php");						//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $link->query("DELETE FROM question_and_answer WHERE id=".$id);						//διαγραφή ερωτήματος από τον πίνακα question_and_answer
 $link->close();												//κλείσιμο σύνδεσης με βάση
 echo "<script> alert('Το ερώτημα διαγράφηκε.'); location.href = 'test.php'; </script>";			//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα test.php
