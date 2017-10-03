980df35f8628ac4129bcadf2ae6892a4339210ae
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2017-10-03 22:42:30 +0300

    Update show_chapter.php

--- code/professor/show_chapter.php
+++ code/professor/show_chapter.php
@@ -15,7 +15,7 @@
 <body>
 	<button class="logout" onclick="logout()"> Αποσύνδεση </button>
 	<img src="../banner.png" alt="Ιστορία Δ' Δημοτικού Στα Αρχαία Χρόνια" class="banner">
-	<div class="menu">
+	<div class="big menu">
 		<span class="menup"> <a href="phome.php" class="link_to_page"> Αρχική </a> </span>
 		<span class="menup"> <a href="message.php" class="link_to_page"> Μηνύματα </a> </span>
 		<span class="menup"> <a href="content.php" class="link_to_page"> Διδακτικό περιεχόμενο </a> </span>
@@ -34,13 +34,14 @@ else {												//αν δεν υπάρχουν
 	echo "<script> alert('Κάτι πήγε στραβά.'); location.href = 'content.php'; </script>";	//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα content.php
 	exit();											//τερματισμός script
 }
-echo "Προβολή | <a href='modify_chapter_form.php?section=".$section."&chapter=".$chapter."'> Επεξεργασία </a> <br> <br>";
+echo "<span class='big'> Προβολή | <a href='modify_chapter_form.php?section=".$section."&chapter=".$chapter."'> Επεξεργασία </a> </span> <br> <br>";
 $link = connect_to_database("../login_register_form.php");					//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
 $result = $link->query("SELECT * FROM chapter WHERE section_number=".$section." AND number=".$chapter);
 												//ανάκτηση στοιχείων κεφαλαίου από τον πίνακα chapter
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 if (!empty($row)) {										//αν υπάρχει το κεφάλαιο
-	echo $row["section_number"].".".$row["number"]." ".$row["title"];			//εμφάνιση του κεφαλαίου
+	echo "<span class='big'>".$row["section_number"].".".$row["number"]." ".$row["title"]."</span>";
+												//εμφάνιση του κεφαλαίου
 	echo "<hr> <br>";
 	$text = "<p class='text'>".$row["text"]."</p>";						//προσθήκη html tags για να φαίνεται όπως πρέπει στην σελίδα
 	$text = str_replace("\n", "\n</p> <p class='text'>", $text);
@@ -54,7 +55,7 @@ if (!empty($row)) {										//αν υπάρχει το κεφάλαιο
 	else {
 		$result = $link->query("SELECT * FROM chapter INNER JOIN material ON chapter.image=material.path WHERE section_number=".$section." AND number=".$chapter);
 		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-		$output = "<div class='chapter_container'> <div class='chapter_image_left'> <img src='".$row["image"]."' class='chapter_image'>".
+		$output = "<div class='container'> <div class='chapter_image_left'> <img src='".$row["image"]."' class='chapter_image'>".
 				"<p class='center'>".$row["description"]."</p> </div> <div class='chapter_text'>".
 				$text."</div> </div> <br>";
 		echo $output;
