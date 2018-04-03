dbb0220e97e29fe3cefbfaf5511bb0440f11fc82
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-04-03 16:20:51 +0300

    Update insert_link.php

--- code/professor/insert_link.php
+++ code/professor/insert_link.php
@@ -9,22 +9,14 @@ else {											//αν δεν υπάρχει
 											//εμφάνιση κατάλληλου μηνύματος και επιστροφή στη σελίδα project.php
 	exit();										//τερματισμός script
 }
-$search_1 = strpos($_POST["link"], "http://");
-$search_2 = strpos($_POST["link"], "https://");
-if ($search_1 === false and $search_2 === false) {					//αν δεν βρέθηκαν τα http:// ή https://
-	$page_link = "http://".$_POST["link"];						//πρόσθεσε http:// στην αρχή της συμβολοσειράς
-}
-else {											//αλλιώς
-	$page_link = $_POST["link"];							//ανάθεση της σε μεταβλητή
-}
-$file_headers = get_headers($page_link);						//καταχώριση επικεφαλίδων που επιστρέφει ο server σε μεταβλητή
-if (!$file_headers or ($file_headers[0] == "HTTP/1.1 404 Not Found")) {			//αν επέστρεψε σφάλμα ότι δεν βρέθηκε
+$file_headers = get_headers($_POST["link"]);						//ανάθεση επικεφαλίδων που επιστρέφει ο server σε μεταβλητή
+if (!$file_headers or ($file_headers[0] == "HTTP/1.1 404 Not Found")) {			//αν επιστροφή σφάλματος ότι δεν βρέθηκε
 	header("Location: project.php?id=".$id."&fail=1");				//επιστροφή στη σελίδα project.php
 }
 else {											//αλλιώς
 	include "../connect_to_database.php";
 	$link = connect_to_database("../login_register_form.php");			//κλήση συνάρτησης για σύνδεση στη βάση δεδομένων
-	$link->query("INSERT INTO link (id, project_id, user, url, description) VALUES (DEFAULT, ".$id.", '".$professor_username."', '".$page_link."', '".$_POST["description"]."')");
+	$link->query("INSERT INTO link (id, project_id, user, url, description) VALUES (DEFAULT, ".$id.", '".$professor_username."', '".$_POST["link"]."', '".$_POST["description"]."')");
 											//εισαγωγή συνδέσμου στον πίνακα link
 	$link->close();									//κλείσιμο σύνδεσης με βάση
 	echo "<script> alert('Ο σύνδεσμος προστέθηκε.'); location.href = 'project.php?id=".$id."'; </script>";
