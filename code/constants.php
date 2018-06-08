3cffb3d5554e4baa03ec4fcb6ead197bc90f608f
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-06-08 12:14:15 +0300

    Create constants.php

---
+++ code/constants.php
@@ -0,0 +1,6 @@
+define ("DATABASE_HOST_NAME", "localhost");
+define ("DATABASE_USERNAME", "root");
+define ("DATABASE_PASSWORD", "");
+define ("DATABASE_NAME", "diplomatiki");
+
+define ("PROFESSOR_USERNAME", "aggelos24");
