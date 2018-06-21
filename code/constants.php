362374038b7d77b52aba19194909777a46d686cf
Angelos Arnaoutis <angelos.arnaoutis@hotmail.com>
2018-06-21 09:57:36 +0300

    Update constants.php

--- code/constants.php
+++ code/constants.php
@@ -1,6 +1,18 @@
-define("DATABASE_HOST_NAME", "localhost");
-define("DATABASE_USERNAME", "root");
-define("DATABASE_PASSWORD", "");
-define("DATABASE_NAME", "diplomatiki");
+<>?php
+if (defined("DATABASE_HOST_NAME") == false) {
+        define("DATABASE_HOST_NAME", "localhost");
+}
+if (defined("DATABASE_USERNAME") == false) {
+        define("DATABASE_USERNAME", "root");
+}
+if (defined("DATABASE_PASSWORD) == false) {
+        define("DATABASE_PASSWORD", "");
+}
+if (defined("DATABASE_NAME") == false) {
+        define("DATABASE_NAME", "diplomatiki");
+}
 
-define("PROFESSOR_USERNAME", "aggelos24");
+if (defined("PROFESSOR_USERNAME") == false) {
+        define("PROFESSOR_USERNAME", "aggelos24");
+}
+?>
